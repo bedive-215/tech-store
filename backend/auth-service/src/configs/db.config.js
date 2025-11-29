00b1c00e249ebd4f5e62b5373eaf6e58a02c58ce
bedive <trungbed1ve@gmail.com>
2025-11-29 23:31:08 +0700

    feat: add database and cloudinary configuration

---
+++ backend/auth-service/src/configs/db.config.js
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+import "dotenv/config"
+// URL format:
+// mysql://username:password@host:port/database
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+export default sequelize;
