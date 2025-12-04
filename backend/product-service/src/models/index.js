ba88aa9a793060a3974caeaf3dc409d1c85efd7d
bedive <trungbed1ve@gmail.com>
2025-12-04 22:40:30 +0700

    chord: index.js models

--- backend/product-service/src/models/index.js
+++ backend/product-service/src/models/index.js
@@ -4,8 +4,8 @@ import brandModel from "./brand.model.js";
 import categoryModel from "./category.model.js";
 import productMediaModel from "./productMedia.model.js";
 import productModel from "./product.model.js";
-import flashSaleModel from "./flashSale.model";
-import flashSaleItemModel from "./flashSaleItem.model";
+import flashSaleModel from "./flashSale.model.js";
+import flashSaleItemModel from "./flashSaleItem.model.js";
 
 const Brand = brandModel(sequelize);
 const Category = categoryModel(sequelize);
