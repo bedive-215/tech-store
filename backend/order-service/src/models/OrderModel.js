7b8a108d0197e778abbf50b1ebdb0bfd4c9d87b1
Dinhtrong1210 <phandinhtronghk2004@gmail.com>
2025-12-09 20:13:20 +0700

    feat(order-service): update OrderModel

--- backend/order-service/src/models/OrderModel.js
+++ backend/order-service/src/models/OrderModel.js
@@ -1,11 +1,11 @@
-// src/models/OrderModel.js
 class OrderModel {
-  constructor({ id, user_id, status, total_price, discount_amount, created_at, updated_at, shipping_address }) {
+  constructor({ id, user_id, status, total_price, discount_amount, final_price, created_at, updated_at, shipping_address }) {
     this.id = id;
     this.user_id = user_id;
     this.status = status;
     this.total_price = total_price;
     this.discount_amount = discount_amount;
+    this.final_price = final_price;   // thêm dòng này
     this.created_at = created_at;
     this.updated_at = updated_at;
     this.shipping_address = shipping_address;
