1b436344384463fee3511f5f617b9234e36a28a6
bedive <trungbed1ve@gmail.com>
2025-12-10 22:11:30 +0700

    feat: add payment database

---
+++ database/schemas/payment_service_db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE payments (
+  id VARCHAR(36) PRIMARY KEY,
+  order_id VARCHAR(36) NOT NULL,
+  user_id VARCHAR(36) NOT NULL,
+  txn_ref VARCHAR(64) DEFAULT NULL UNIQUE,
+  amount BIGINT NOT NULL,
+  currency VARCHAR(8) DEFAULT 'VND',
+  status ENUM('pending', 'success', 'failed', 'refunded')NOT NULL DEFAULT 'pending',
+  transaction_id VARCHAR(128),
+  metadata JSON DEFAULT NULL,
+  created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
